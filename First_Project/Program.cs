a302111d060a3a96b35ad9a54453721224cb80ac
Luka Vogt <luka.vogt.dev@outlook.de>
2026-02-24 21:33:08 +0100

    Initial character stats for Grimm the Barbarian

--- First_Project/Program.cs
+++ First_Project/Program.cs
@@ -4,12 +4,33 @@
     {
         static void Main(string[] args)
         {
-            
-            
+            // Character Profile
+            string name = "Grimm";
+            string race = "Half-Orc";
+            string characterClass = "Barbar";
+            string background = "Soldier";
+
+            // Attributes
+            int strength = 17;
+            int dexterity = 12;
+            int health = 15;
+            int level = 1;
+            int armorClass = 10;
+
+            // Skills
+            int athletics = 5;
+            int survival = 3;
+
+            // Equipment
+            string mainWeapon = "Combataxe";
+            int weaponDamage = 9;
+            int gold = 10;
+
+            // Status
+            bool isAlive = true;
 
 
-            
-            
+
 
 
 
