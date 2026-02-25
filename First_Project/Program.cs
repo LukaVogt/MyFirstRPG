59187dbcdcec0348943d5ecabfdeab17210a1009
Luka Vogt <luka.vogt.dev@outlook.de>
2026-02-25 13:49:01 +0100

    First full gameplay loop: Tracking, Combat and HP reduction logic.

--- First_Project/Program.cs
+++ First_Project/Program.cs
@@ -64,8 +64,18 @@
             Console.WriteLine("Gamestart");
             Console.WriteLine();
 
+            // Enemy(Goblin)
+            int goblinHealth = 11;
+            int goblinArmorClass = 12;
+            int goblinStrength = 2;
+            int goblinDamage = 5;
+
+            
+
+
+
             // Start
-            Console.WriteLine("Grimm is standing in a mud pit and is looking for a tail");
+            Console.WriteLine("Grimm is standing in a mud pit and is looking for a trail");
             int survivalthrow = d20.Next(1, 21) + survival;
             Console.WriteLine("Survivalthrow");
             Console.WriteLine("You need a 14");
@@ -73,9 +83,39 @@
             
             if(survivalthrow >= 14)
             {
+                // Stortelling
                 Console.WriteLine("Succeed! The trail goes to the east.");
+                Console.WriteLine("You followed the muddy tracks for a while...");
+                Console.WriteLine("Suddenly you hear a rustle in a bush.");
+                Console.WriteLine("A small, green Goblin stares at you with yellow eyes!");
+                Console.WriteLine("It jumps out and waves with its curved sword. That means fight!");
 
-            }else
+                Console.WriteLine();
+
+                // Fight
+
+                Console.WriteLine("Grimm takes 3 steps closer and takes a swing");
+                int strengthThrow = d20.Next(1, 21) + athletics;
+                Console.WriteLine("Strengththrow!");
+                Console.WriteLine("You need a " + goblinArmorClass);
+                Console.WriteLine("You got a " + strengthThrow);
+
+                if (strengthThrow >= goblinArmorClass) {
+                    Console.WriteLine("Succeed!");
+                    Console.WriteLine("You land a brutal shot and the goblin now bleeding from its side");
+                     goblinHealth = goblinHealth - weaponDamage;
+                    Console.WriteLine("The Goblin has " + goblinHealth + "left");
+
+                }else
+                {
+                    Console.WriteLine("Fail!");
+                    Console.WriteLine("It doges and you miss by inches and are now open for a counter");
+                }
+
+
+
+            }
+            else
             {
                 Console.WriteLine("Fail! Grimm just looks at an old rock");
             }
